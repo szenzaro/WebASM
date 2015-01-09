2067c531856719580cb6bd151fa62c0683123d7f
Simone Zenzaro <johnnyfreak@dyd.local>
2015-01-09 15:51:05 +0100

    First commit

---
+++ src/main/java/WebASM.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Simone Zenzaro on 09/01/15.
+ */
+
+import  org.coreasm.engine.CoreASMEngine;
+
+public class WebASM {
+    public static void main(String[] args){
+        System.out.println("Hello " + CoreASMEngine.class.toString());
+    }
+}
