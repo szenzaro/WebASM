1a29d7fa8cc20803843602747c3ce5696ff81883
Simone Zenzaro <johnnyfreak@dhcp-131-114-88-229.di.unipi.it>
2015-01-12 11:18:28 +0100

    added ide web page, starting testing applet

---
+++ src/main/java/org/coreasm/webasm/WebASM.java
@@ -0,0 +1,38 @@
+package org.coreasm.webasm;
+
+/**
+ * Created by Simone Zenzaro on 09/01/15.
+ */
+
+import  org.coreasm.engine.CoreASMEngine;
+import org.coreasm.engine.CoreASMEngineFactory;
+
+import java.applet.Applet;
+import java.awt.*;t
+
+public class WebASM extends Applet{
+
+    CoreASMEngine engine = null;
+
+    public void init(){
+        engine = CoreASMEngineFactory.createEngine();
+        System.out.println("Inizio");
+    }
+
+    public void paint(Graphics g){
+        System.out.println("Paint");
+        g.setColor(Color.blue);
+        g.drawRect(0, 0,
+                getSize().width -1,
+                getSize().height -1);
+        g.setColor(Color.red);
+        g.drawString("asd: " + CoreASMEngine.class.getName(), 15, 25);
+    }
+
+    public String hello() { return "hello"; }
+
+    public String testApplet(){
+        return engine.getEngineMode().toString();
+    }
+
+}
